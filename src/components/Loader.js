e03307692c307c96816a35f320568246c13ee55f
Cesar <cesar_sevilla@outlook.com>
2023-01-06 17:38:33 -0600

    Add Loader Component

---
+++ src/components/Loader.js
@@ -0,0 +1,17 @@
+
+const Loader = () => {
+  return (
+    <div id="loader">
+        <div  className="browser-screen-loading-content">
+            <div className="loading-dots dark-gray">
+                <i></i>
+                <i></i>
+                <i></i>
+                <i></i>
+            </div>
+        </div>
+    </div>
+  );
+};
+
+export default Loader;
