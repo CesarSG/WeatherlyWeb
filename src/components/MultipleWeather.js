11f58d6fa6672175aa6d661c3b4b87b86c6f4daf
Cesar <cesar_sevilla@outlook.com>
2023-01-09 16:22:41 -0600

    Fx MultipleWeather: fix styles

--- src/components/MultipleWeather.js
+++ src/components/MultipleWeather.js
@@ -1,5 +1,5 @@
 import Loader from "./Loader";
-import { getFormatTime } from "../utils";
+import { getImageWeather, getFormatTime } from "../utils";
 import { Fragment } from "react";
 
 const MultipleWeather = (props) => {
@@ -12,20 +12,28 @@ const MultipleWeather = (props) => {
     return (
         <>
             { isLoading ? (<Loader />) : ( 
-                <div className="container my-5">
-                    <div className="row">
-                        <div className="col-12">
-                            <h3>Multiple</h3>
-                            {multipleAPI.data.list.map((item, index) => {
-                                return (
-                                    <Fragment key={index}>
-                                        <p>City: {item.name}</p>
-                                    </Fragment>
-                                )
-                            })}
-                        </div>
-                    </div>
-                </div>
+                <Fragment>
+                    <h3>Other cities</h3>
+                    {multipleAPI.data.list.map((item, index) => {
+                        return (
+                            <div key={index} className="my-3 py-4 bg-card row">
+                                <div className="col-4 text-center">
+                                    <img className="px-2" src={getImageWeather(item.weather[0].icon)} />
+                                    <p className="fw-bold">{item.main.temp}°</p>
+                                </div>
+                                <div className="col-4 px-1">
+                                    <p>{item.sys.country}</p>
+                                    <p>{item.name}</p>
+                                    <p>Real feel: {item.main.feels_like}°</p>
+                                    <p>{item.weather[0].description}</p>
+                                </div>
+                                <div className="col-4 d-flex text-center align-items-center">
+                                    <p className="fw-bold">{item.main.temp_min}° / {item.main.temp_max}°</p>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </Fragment>
             )}
         </>
     );
