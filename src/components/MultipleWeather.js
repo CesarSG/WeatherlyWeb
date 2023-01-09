38b1b8af3712b877ba90cb534e670078b6e185bf
Cesar <cesar_sevilla@outlook.com>
2023-01-09 16:38:42 -0600

    Fx MultipleWeather: add implementation getTemperatureFormat

--- src/components/MultipleWeather.js
+++ src/components/MultipleWeather.js
@@ -1,5 +1,5 @@
 import Loader from "./Loader";
-import { getImageWeather, getFormatTime } from "../utils";
+import { getImageWeather, getTemperatureFormat } from "../utils";
 import { Fragment } from "react";
 
 const MultipleWeather = (props) => {
@@ -17,9 +17,11 @@ const MultipleWeather = (props) => {
                     {multipleAPI.data.list.map((item, index) => {
                         return (
                             <div key={index} className="my-3 py-4 bg-card row">
-                                <div className="col-4 text-center">
-                                    <img className="px-2" src={getImageWeather(item.weather[0].icon)} />
-                                    <p className="fw-bold">{item.main.temp}°</p>
+                                <div className="col-4 text-center d-flex text-center align-items-center">
+                                    <div>
+                                        <img alt={item.weather[0].description} className="px-2" src={getImageWeather(item.weather[0].icon)} />
+                                        <p className="fw-bold">{getTemperatureFormat(item.main.temp)}</p>
+                                    </div>
                                 </div>
                                 <div className="col-4 px-1">
                                     <p>{item.sys.country}</p>
@@ -27,8 +29,8 @@ const MultipleWeather = (props) => {
                                     <p>Real feel: {item.main.feels_like}°</p>
                                     <p>{item.weather[0].description}</p>
                                 </div>
-                                <div className="col-4 d-flex text-center align-items-center">
-                                    <p className="fw-bold">{item.main.temp_min}° / {item.main.temp_max}°</p>
+                                <div className="col-4 d-flex text-center align-items-center justify-content-center">
+                                    <p className="fw-bold">{getTemperatureFormat(item.main.temp_min)} / {getTemperatureFormat(item.main.temp_max)}</p>
                                 </div>
                             </div>
                         )
