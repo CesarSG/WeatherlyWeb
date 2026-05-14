7511232967c070d325fbf57065e61616b608dcb3
Cesar Sevilla <cesar_sevilla@outlook.com>
2026-05-14 10:28:12 -0500

    refactor: simplify Main component by integrating useWeather hook

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,107 +1,40 @@
-import { useEffect, useReducer, useState } from "react";
-import { API } from "../utils";
 import { useTheme } from "../context/ThemeContext";
-import DataReducer from "../reducer/DataReducer";
+import useWeather from "../hooks/useWeather";
 import CurrentWeather from "./CurrentWeather";
 import ForecastWeather from "./ForecastWeather";
 import MultipleWeather from "./MultipleWeather";
 
 const Main = () => {
-
-    const initialState = {
-        data: [],
-    };
-
     const { unit, city } = useTheme();
-
-    const [isLoadingCurrent, setIsLoadingCurrent] = useState(true);
-    const [isLoadingForecast, setIsLoadingForecast] = useState(true);
-    const [isLoadingMultiple, setIsLoadingMultiple] = useState(true);
-    const [currentAPI, dispatchCurrent] = useReducer(DataReducer, initialState);
-    const [forecastAPI, dispatchForecast] = useReducer(DataReducer, initialState);
-    const [multipleAPI, dispatchMultiple] = useReducer(DataReducer, initialState);
-
-    useEffect(() => {
-        // Set loader as true
-        setIsLoadingCurrent(true);
-        setIsLoadingForecast(true);
-
-        fetch(API.current + city + API.units + unit + API.id + API.key)
-            .then((response) => response.json())
-            .then((data) => dispatchCurrent({ payload: data }))
-            .catch((error) => console.log("Current: " + error))
-
-        fetch(API.forecast + city + API.units + unit + API.id + API.key)
-            .then((response) => response.json())
-            .then((data) => dispatchForecast({ payload: data }))
-            .catch((error) => console.log("Forecast: " + error))
-    },[city]);
-
-    useEffect(() => {
-        // Set loader as true
-        setIsLoadingCurrent(true);
-        setIsLoadingForecast(true);
-
-        fetch(API.current + city + API.units + unit + API.id + API.key)
-            .then((response) => response.json())
-            .then((data) => dispatchCurrent({ payload: data }))
-            .catch((error) => console.log("Current: " + error))
-
-        fetch(API.forecast + city + API.units + unit + API.id + API.key)
-            .then((response) => response.json())
-            .then((data) => dispatchForecast({ payload: data }))
-            .catch((error) => console.log("Forecast: " + error))
-
-        fetch(API.multiple + API.units + unit + API.id + API.key)
-            .then((response) => response.json())
-            .then((data) => dispatchMultiple({ payload: data }))
-            .catch((error) => console.log("Multiple: " + error))
-    },[unit]);
-
-    useEffect(() => {
-        // Update isLoading current API
-        if(Object.keys(currentAPI.data).length ){
-            setIsLoadingCurrent(false)
-        } 
-        
-        // Update isLoading forecast API
-        if(Object.keys(forecastAPI.data).length ){
-            setIsLoadingForecast(false)
-        }
-        
-        // Update isLoading multiple API
-        if(Object.keys(multipleAPI.data).length ){
-            setIsLoadingMultiple(false)
-        }
-    }, [currentAPI, forecastAPI, multipleAPI])
-
-    useEffect(() => {
-        console.log(currentAPI)             
-    }, [currentAPI])
+    const {
+        currentAPI,  isLoadingCurrent,
+        forecastAPI, isLoadingForecast,
+        multipleAPI, isLoadingMultiple,
+    } = useWeather(city, unit);
 
     return (
         <div id="main">
             <div className="container">
                 <div className="row py-5">
                     <div className="col-12 col-lg-8">
-                        <CurrentWeather 
+                        <CurrentWeather
                             isLoading={isLoadingCurrent}
                             currentAPI={currentAPI}
                         />
-                        {<ForecastWeather 
+                        <ForecastWeather
                             isLoading={isLoadingForecast}
                             forecastAPI={forecastAPI}
                             showItems={5}
-                        />}
+                        />
                     </div>
                     <div className="col-12 col-lg-4">
-                        <MultipleWeather 
+                        <MultipleWeather
                             isLoading={isLoadingMultiple}
                             multipleAPI={multipleAPI}
-                        /> 
+                        />
                     </div>
                 </div>
-            </div>     
+            </div>
         </div>
     );
 };
